98ce43c81bfbcfd91e29c45ca02867a3018ffd38
hatim77100 <najoua-hatim@hotmail.fr>
2022-12-10 23:42:07 +0100

    add barre de recherche

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -3,15 +3,20 @@ import Recipe from "./Recipe";
 import { data } from "../data/recipes";
 
 function Content() {
-
   const recipes = data;
 
   return (
     <div className="flex-fill container">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-      <div className="card p-20 contentCard br">
+      <div className="card d-flex flex-column p-20 contentCard br">
+        <div className={`d-flex flex-row justify-center align-center my-30 ${styles.searchBar}`}>
+          <i icon="fa-sharp fa-solid fa-magnifying-glass mr-15"></i>
+          <input className="flex-fill" type="text" placeholder="Rechercher" />
+        </div>
         <div className={styles.grid}>
-          {recipes.map( r => (<Recipe key={r._id}  title={r.title} image={r.image}/>))}
+          {recipes.map((r) => (
+            <Recipe key={r._id} title={r.title} image={r.image} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,33 @@
 import styles from "./Header.module.scss";
 import logo from "../../assets/images/logo.png"
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+
+    const [showMenu, setShowMenu] = useState(false);
+
+
+
     return (
         <header className={`${styles.header} d-flex flex-row align-center justify-center`}>
-        <i className="fa-regular fa-bars"></i>
         <div className="flex-fill">
             <img src={logo} alt="logo bonne appétit" width="100px"/>
         </div>
-        <ul>
+        <ul className={styles.headerlist}>
             <button className="mr-5 btn btn-reverse-primary">
-            <i className="fa-regular fa-basket-shopping"></i>
-                <span>panier</span></button>
+            <i className="fa-regular fa-heart mr-5"></i>
+                <span>Wishlist</span></button>
             <button className="btn btn-primary">connexion</button>
         </ul>
 
+        <i onClick={() => setShowMenu(true)} className={`fa-regular fa-bars ${styles.headerXs}`}></i>
+       { showMenu && 
+       <>
+       <div onClick={() => setShowMenu(false)} className="calc"></div>
+        <HeaderMenu/>
+       </>
+        }
     </header>
     )
 }
